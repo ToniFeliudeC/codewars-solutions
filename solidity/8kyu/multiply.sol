be36570712974c9e89634489aaf6a4ad1a4aad65
ToniFeliudeC <afeliusalas@cifpfbmoll.eu>
2022-09-02 16:22:01 +0200

    Solidity folder and katas Added

---
+++ solidity/8kyu/multiply.sol
@@ -0,0 +1,12 @@
+//This code does not execute properly.
+//Try to figure out why.
+
+//My solution
+
+pragma solidity >=0.4.19;
+
+contract DummyToken {
+  function multiply(int a, int b) pure public returns (int) {
+    return a * b;
+  }
+}
